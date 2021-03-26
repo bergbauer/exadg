37b513fae7c551a47b87a50210f2aeb4131c7edd
bergbauer <bergbauer@users.noreply.github.com>
2021-03-26 15:48:00 +0000

    deploy: a4010c030e7287a07b86da24c5de181705a131c2

--- search/all_a.js
+++ search/all_a.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['kineticenergycalculator_130',['KineticEnergyCalculator',['../classExaDG_1_1KineticEnergyCalculator.html',1,'ExaDG']]],
-  ['kineticenergycalculatordetailed_131',['KineticEnergyCalculatorDetailed',['../classExaDG_1_1IncNS_1_1KineticEnergyCalculatorDetailed.html',1,'ExaDG::IncNS']]],
-  ['kineticenergydata_132',['KineticEnergyData',['../structExaDG_1_1KineticEnergyData.html',1,'ExaDG']]],
-  ['kineticenergyspectrumcalculator_133',['KineticEnergySpectrumCalculator',['../classExaDG_1_1KineticEnergySpectrumCalculator.html',1,'ExaDG']]],
-  ['kineticenergyspectrumdata_134',['KineticEnergySpectrumData',['../structExaDG_1_1KineticEnergySpectrumData.html',1,'ExaDG']]]
+  ['kineticenergycalculator_121',['KineticEnergyCalculator',['../classExaDG_1_1KineticEnergyCalculator.html',1,'ExaDG']]],
+  ['kineticenergycalculatordetailed_122',['KineticEnergyCalculatorDetailed',['../classExaDG_1_1IncNS_1_1KineticEnergyCalculatorDetailed.html',1,'ExaDG::IncNS']]],
+  ['kineticenergydata_123',['KineticEnergyData',['../structExaDG_1_1KineticEnergyData.html',1,'ExaDG']]],
+  ['kineticenergyspectrumcalculator_124',['KineticEnergySpectrumCalculator',['../classExaDG_1_1KineticEnergySpectrumCalculator.html',1,'ExaDG']]],
+  ['kineticenergyspectrumdata_125',['KineticEnergySpectrumData',['../structExaDG_1_1KineticEnergySpectrumData.html',1,'ExaDG']]]
 ];
--- search/classes_9.js
+++ search/classes_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['jacobipreconditioner_439',['JacobiPreconditioner',['../classExaDG_1_1JacobiPreconditioner.html',1,'ExaDG']]],
-  ['jacobismoother_440',['JacobiSmoother',['../classExaDG_1_1JacobiSmoother.html',1,'ExaDG']]]
+  ['jacobipreconditioner_425',['JacobiPreconditioner',['../classExaDG_1_1JacobiPreconditioner.html',1,'ExaDG']]],
+  ['jacobismoother_426',['JacobiSmoother',['../classExaDG_1_1JacobiSmoother.html',1,'ExaDG']]]
 ];
