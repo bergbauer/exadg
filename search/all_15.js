7736bca8677f492d68ecd600bdfb25ed6cb3174b
bergbauer <bergbauer@users.noreply.github.com>
2021-12-20 16:28:02 +0000

    deploy: 0af19c5042ba32046aa97b3852c84e1e69a79446

--- search/all_15.js
+++ search/all_15.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['variablecoefficients_314',['VariableCoefficients',['../classExaDG_1_1VariableCoefficients.html',1,'ExaDG']]],
-  ['variablecoefficientscells_315',['VariableCoefficientsCells',['../classExaDG_1_1VariableCoefficientsCells.html',1,'ExaDG']]],
-  ['velocitymagnitudecalculator_316',['VelocityMagnitudeCalculator',['../classExaDG_1_1IncNS_1_1VelocityMagnitudeCalculator.html',1,'ExaDG::IncNS']]],
-  ['velocityprojection_317',['VelocityProjection',['../classExaDG_1_1VelocityProjection.html',1,'ExaDG']]],
-  ['viscouskernel_318',['ViscousKernel',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html',1,'ExaDG::IncNS::Operators']]],
-  ['viscouskerneldata_319',['ViscousKernelData',['../structExaDG_1_1IncNS_1_1Operators_1_1ViscousKernelData.html',1,'ExaDG::IncNS::Operators']]],
-  ['viscousoperator_320',['ViscousOperator',['../classExaDG_1_1CompNS_1_1ViscousOperator.html',1,'ExaDG::CompNS::ViscousOperator&lt; dim, Number &gt;'],['../classExaDG_1_1IncNS_1_1ViscousOperator.html',1,'ExaDG::IncNS::ViscousOperator&lt; dim, Number &gt;']]],
-  ['viscousoperatordata_321',['ViscousOperatorData',['../structExaDG_1_1CompNS_1_1ViscousOperatorData.html',1,'ExaDG::CompNS::ViscousOperatorData&lt; dim &gt;'],['../structExaDG_1_1IncNS_1_1ViscousOperatorData.html',1,'ExaDG::IncNS::ViscousOperatorData&lt; dim &gt;']]],
-  ['vorticitycalculator_322',['VorticityCalculator',['../classExaDG_1_1CompNS_1_1VorticityCalculator.html',1,'ExaDG::CompNS::VorticityCalculator&lt; dim, Number &gt;'],['../classExaDG_1_1IncNS_1_1VorticityCalculator.html',1,'ExaDG::IncNS::VorticityCalculator&lt; dim, Number &gt;']]]
+  ['variablecoefficients_0',['VariableCoefficients',['../classExaDG_1_1VariableCoefficients.html',1,'ExaDG']]],
+  ['variablecoefficientscells_1',['VariableCoefficientsCells',['../classExaDG_1_1VariableCoefficientsCells.html',1,'ExaDG']]],
+  ['velocitymagnitudecalculator_2',['VelocityMagnitudeCalculator',['../classExaDG_1_1IncNS_1_1VelocityMagnitudeCalculator.html',1,'ExaDG::IncNS']]],
+  ['velocityprojection_3',['VelocityProjection',['../classExaDG_1_1VelocityProjection.html',1,'ExaDG']]],
+  ['viscouskernel_4',['ViscousKernel',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html',1,'ExaDG::IncNS::Operators']]],
+  ['viscouskerneldata_5',['ViscousKernelData',['../structExaDG_1_1IncNS_1_1Operators_1_1ViscousKernelData.html',1,'ExaDG::IncNS::Operators']]],
+  ['viscousoperator_6',['ViscousOperator',['../classExaDG_1_1CompNS_1_1ViscousOperator.html',1,'ExaDG::CompNS::ViscousOperator&lt; dim, Number &gt;'],['../classExaDG_1_1IncNS_1_1ViscousOperator.html',1,'ExaDG::IncNS::ViscousOperator&lt; dim, Number &gt;']]],
+  ['viscousoperatordata_7',['ViscousOperatorData',['../structExaDG_1_1CompNS_1_1ViscousOperatorData.html',1,'ExaDG::CompNS::ViscousOperatorData&lt; dim &gt;'],['../structExaDG_1_1IncNS_1_1ViscousOperatorData.html',1,'ExaDG::IncNS::ViscousOperatorData&lt; dim &gt;']]],
+  ['vorticitycalculator_8',['VorticityCalculator',['../classExaDG_1_1CompNS_1_1VorticityCalculator.html',1,'ExaDG::CompNS::VorticityCalculator&lt; dim, Number &gt;'],['../classExaDG_1_1IncNS_1_1VorticityCalculator.html',1,'ExaDG::IncNS::VorticityCalculator&lt; dim, Number &gt;']]]
 ];
--- search/classes_4.js
+++ search/classes_4.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['eigenvaluetracker_386',['EigenvalueTracker',['../structExaDG_1_1EigenvalueTracker.html',1,'ExaDG']]],
-  ['elasticityoperatorbase_387',['ElasticityOperatorBase',['../classExaDG_1_1Structure_1_1ElasticityOperatorBase.html',1,'ExaDG::Structure']]],
-  ['errorcalculationdata_388',['ErrorCalculationData',['../structExaDG_1_1ErrorCalculationData.html',1,'ExaDG']]],
-  ['errorcalculator_389',['ErrorCalculator',['../classExaDG_1_1ErrorCalculator.html',1,'ExaDG']]],
-  ['explicitrungekuttatimeintegrator_390',['ExplicitRungeKuttaTimeIntegrator',['../classExaDG_1_1ExplicitRungeKuttaTimeIntegrator.html',1,'ExaDG']]],
-  ['explicittimeintegrator_391',['ExplicitTimeIntegrator',['../classExaDG_1_1ExplicitTimeIntegrator.html',1,'ExaDG']]],
-  ['extrapolationconstants_392',['ExtrapolationConstants',['../classExaDG_1_1ExtrapolationConstants.html',1,'ExaDG']]]
+  ['eigenvaluetracker_0',['EigenvalueTracker',['../structExaDG_1_1EigenvalueTracker.html',1,'ExaDG']]],
+  ['elasticityoperatorbase_1',['ElasticityOperatorBase',['../classExaDG_1_1Structure_1_1ElasticityOperatorBase.html',1,'ExaDG::Structure']]],
+  ['errorcalculationdata_2',['ErrorCalculationData',['../structExaDG_1_1ErrorCalculationData.html',1,'ExaDG']]],
+  ['errorcalculator_3',['ErrorCalculator',['../classExaDG_1_1ErrorCalculator.html',1,'ExaDG']]],
+  ['explicitrungekuttatimeintegrator_4',['ExplicitRungeKuttaTimeIntegrator',['../classExaDG_1_1ExplicitRungeKuttaTimeIntegrator.html',1,'ExaDG']]],
+  ['explicittimeintegrator_5',['ExplicitTimeIntegrator',['../classExaDG_1_1ExplicitTimeIntegrator.html',1,'ExaDG']]],
+  ['extrapolationconstants_6',['ExtrapolationConstants',['../classExaDG_1_1ExtrapolationConstants.html',1,'ExaDG']]]
 ];
