4c81a8f846c79f8add4016ddbc6509e6b2d3b1fc
bergbauer <bergbauer@users.noreply.github.com>
2022-06-28 14:49:15 +0000

    deploy: 8382b8bacb87b907111ffe60e9cc48d7e86c2a4c

--- menudata.js
+++ menudata.js
@@ -57,9 +57,11 @@ var menudata={children:[
 {text:"p",url:"functions_func.html#index_p"},
 {text:"r",url:"functions_func.html#index_r"},
 {text:"s",url:"functions_func.html#index_s"},
+{text:"t",url:"functions_func.html#index_t"},
 {text:"u",url:"functions_func.html#index_u"},
 {text:"w",url:"functions_func.html#index_w"},
 {text:"~",url:"functions_func.html#index__7E"}]},
-{text:"Variables",url:"functions_vars.html"}]}]},
+{text:"Variables",url:"functions_vars.html"},
+{text:"Typedefs",url:"functions_type.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"}]}]}
